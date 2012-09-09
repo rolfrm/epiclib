fd1579291618cffd2dad58ff1b6b01aa8e413881
Rolf Madsen <rolfrm@gmail.com>
2012-09-09 11:43:20 +0200

    Added possibility to compile shared library and README

--- make.py
+++ make.py
@@ -1,6 +1,6 @@
 import pycompile as pyc
 import sys
 print sys.argv
-
-pyc.Compile("start_demo","","obj",["Graphics/GraphicsCore.cpp","demo.cpp","Physics/PhysicsCore.cpp","Math/Math.cpp","Physics/PhysicsObject.cpp","Physics/Polygon.cpp","Physics/Constraint.cpp","Physics/SeperatingAxis.cpp","Physics/Quadtree.cpp","Physics/QuadDetection.cpp"],["glfw","IL","GL","GLEW"],0,3,pyc.CPPOX,[],
+t = "demo.cpp"
+pyc.CompileSharedLibrary("libepiclib.so","","so_obj",["Graphics/GraphicsCore.cpp","Physics/PhysicsCore.cpp","Math/Math.cpp","Physics/PhysicsObject.cpp","Physics/Polygon.cpp","Physics/Constraint.cpp","Physics/SeperatingAxis.cpp","Physics/Quadtree.cpp","Physics/QuadDetection.cpp"],["glfw","IL","GL","GLEW"],0,3,pyc.CPPOX,[],
             ["./","Physics"])
